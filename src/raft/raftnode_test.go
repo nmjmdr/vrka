96d15b915ac17bc52e4713def3f1ee68f42a616f
nmujumdar <nmujumdar@in.imshealth.com>
2016-05-05 09:12:08 +0530

    added heartbeat test for leader

--- src/raft/raftnode_test.go
+++ src/raft/raftnode_test.go
@@ -338,6 +338,8 @@ func Test_TransitionWithDelayForVoting(t *testing.T) {
 	// signal election notice for node id0
 	monitors[0].c <- time.Time{}
 
+	doneWg.Wait()
+
 	for i:=0;i<n;i++ {
 		nodes[i].Stop()
 	}
@@ -345,7 +347,83 @@ func Test_TransitionWithDelayForVoting(t *testing.T) {
 
 
 func Test_ThreePeersTransitionOtherNodeHeartbeat(t *testing.T) {
-	// to do
+
+	n := 3
+
+	//setup peers
+	peers := make([]Peer,n)
+	for i:=0;i<n;i++ {
+		peers[i].Id = fmt.Sprintf("id%d",i)
+	}
+	config := NewMockConfig(peers)
+
+		
+	monitors := make([](*mockMonitor),n)
+	transports := make([](*mockTransport),n)
+	nodes := make([]RaftNode,n)
+		
+	for i:=0;i<n;i++ {
+		monitors[i] = new(mockMonitor)
+		monitors[i].c = make(chan time.Time)
+
+		// give the vote
+		vres := voteResponse{}
+		vres.voteGranted = false
+		
+		transports[i] = NewMockTransportWithDelay(vres,(500 * time.Millisecond))
+
+		nodes[i] = NewRaftNode(fmt.Sprintf("id%d",i),monitors[i],config,transports[i])
+	
+	}
+
+	
+	doneWg := sync.WaitGroup{}
+	doneWg.Add(1)
+	
+	go func() {		
+		select {
+		case role,_ := <- nodes[0].RoleChange():
+			if role != candidate {
+				t.Fatal("should have been a candidate")
+			}
+		
+		}
+		doneWg.Done()
+	}()
+	
+	
+	
+	// signal election notice for node id0
+	monitors[0].c <- time.Time{}
+
+	doneWg.Wait()
+
+
+	doneWg.Add(1)
+	
+	go func() {		
+		select {
+		case role,_ := <- nodes[0].RoleChange():
+			if role != follower {
+				t.Fatal("should have been a follower")
+			}
+		
+		}
+		doneWg.Done()
+	}()
+
+	// now signal a heartbeat from another node
+	beat := beat{}
+	raft0,_ := nodes[0].(*raftNode)
+	beat.term = (raft0.term + 1)
+	beat.from = "id1"
+	nodes[0].Heartbeat(beat)
+
+	doneWg.Wait()
+
+	for i:=0;i<n;i++ {
+		nodes[i].Stop()
+	}
 }
 
 
